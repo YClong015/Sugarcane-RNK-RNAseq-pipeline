ebcd91acd5108ab9fd7bab3ca83a63db007a378f
Ethan Z <yanchenzheng34@gmail.com>
2026-02-09 02:04:35 +1000

    Update 11_make_sample_metadata_from_list.sh

--- scripts/11_make_sample_metadata_from_list.sh
+++ scripts/11_make_sample_metadata_from_list.sh
@@ -5,14 +5,25 @@ load_cfg
 
 [[ -f "$SAMPLES_RKN_ONLY_TXT" ]] || die "Missing: $SAMPLES_RKN_ONLY_TXT"
 
-OUT_CSV="${DESEQ_DIR}/sample_metadata.csv"
-echo "sample,genotype,time,treatment,rep" > "$OUT_CSV"
+: "${CONTROL_CODE:?Missing CONTROL_CODE in config.env}"
+: "${CONTROL_LABEL:?Missing CONTROL_LABEL in config.env}"
+: "${CASE_CODE:?Missing CASE_CODE in config.env}"
+: "${CASE_LABEL:?Missing CASE_LABEL in config.env}"
 
-awk -F'_' 'BEGIN{OFS=","}
+OUT_CSV="${DESEQ_DIR}/sample_metadata.csv"
+echo "sample_id,genotype,time_point,treatment,replicate" > "$OUT_CSV"
+
+awk -F'_' -v cc="$CONTROL_CODE" -v cl="$CONTROL_LABEL" \
+  -v kc="$CASE_CODE" -v kl="$CASE_LABEL" 'BEGIN{OFS=","}
 {
   rep=$4;
   for(i=5;i<=NF;i++) rep=rep"_"$i;
-  print $0,$1,$2,$3,rep
+
+  tr=$3;
+  if (tr == cc) tr = cl;
+  if (tr == kc) tr = kl;
+
+  print $0,$1,$2,tr,rep
 }' "$SAMPLES_RKN_ONLY_TXT" >> "$OUT_CSV"
 
 head "$OUT_CSV" || true
